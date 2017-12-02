0b584361b4aa2015301e20c038f2edf91b6c7b50
Henry Ho <henryho206@gmail.com>
2017-12-02 01:12:12 -0800

    Build Matchup objects from a hardcoded API request.

--- espnff.gemspec
+++ espnff.gemspec
@@ -9,8 +9,8 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Henry Ho"]
   spec.email         = ["henryho206@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
+  spec.summary       = %q{Client wrapping ESPN's Fantasy Football API.}
+  spec.description   = %q{Client wrapping ESPN's Fantasy Football API.}
   spec.homepage      = "TODO: Put your gem's website or public repo URL here."
   spec.license       = "MIT"
 
@@ -33,4 +33,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "minitest", "~> 5.0"
+  spec.add_runtime_dependency "rest-client"
+  spec.add_runtime_dependency "pry-byebug" # TODO: change this shit
 end
--- lib/espnff.rb
+++ lib/espnff.rb
@@ -2,4 +2,56 @@ require "espnff/version"
 
 module Espnff
   # Your code goes here...
+  def self.league_schedules
+    require 'rest-client'
+    require 'json'
+    require 'ostruct'
+
+    # "http://games.espn.com/ffl/api/v2/leagueSchedules?leagueId=511167&seasonId=2016"
+    url = "http://games.espn.com/ffl/api/v2/leagueSchedules?leagueId=511167"
+    response = RestClient.get(url)
+    JSON.parse(response)
+  end
+
+  def self.output_matchups
+    matchups = []
+    # require 'pry-byebug'
+    # binding.pry
+    #
+    league_schedules = self.league_schedules
+    weeks = league_schedules["leagueSchedule"]["scheduleItems"]
+    weeks.each do |week|
+      week["matchups"].each do |matchup|
+        matchups << Matchup.new(matchup)
+      end
+    end
+
+    p matchups.inspect
+    puts matchups.length
+  end
+
+  class Matchup
+    attr_accessor :away_team_score, :away_team_id, :home_team_score, :home_team_id, :result
+
+    def initialize(json)
+      hash = parse_json(json)
+      self.away_team_score = hash[:away_team_score]
+      self.away_team_id = hash[:away_team_id]
+      self.home_team_score = hash[:home_team_score]
+      self.home_team_id = hash[:home_team_id]
+      self.result = hash[:result]
+    end
+
+    def parse_json(json)
+      hash = {}
+
+      hash[:away_team_score] = json["awayTeamScores"].first
+      hash[:away_team_id] = json["awayTeam"]["teamId"]
+      hash[:home_team_score] = json["homeTeamScores"].first
+      hash[:home_team_id] = json["homeTeam"]["teamId"]
+      hash[:result] = json["outcome"]
+
+      hash
+    end
+  end
 end
